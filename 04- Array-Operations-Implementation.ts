467c705f6b8f0c4bc03dfe6297f7b12140a49868
Mohamed Yahia <97320765+0xYahia@users.noreply.github.com>
2023-08-30 18:33:19 +0300

    array template

---
+++ 04- Array-Operations-Implementation.ts
@@ -0,0 +1,28 @@
+//! 04 – Array – Operations Implementation
+//! Array implement some operation for Ts
+
+//! Template:
+//! 1) Name: Resize
+
+//! 2) Assumptions
+//      - Array data is stored in memory heap
+//      - Array address is stored in memory stack
+
+//! 3) Inputs:
+//      - The array data type
+//      - The array itself
+//      - New size
+
+//! 4) Processes: Must be first on the function
+//      1) Validations:
+//          - The new size is a valid number
+//          - The new size is not equal to the current size
+//          - The array is not null
+
+//      2) Resize the array:
+//          - Create a new empty array from the same type with the new size
+//          - Move all items from the source array to the new array
+//          - Assign the address of the new array for the source array address
+
+//! 5) Outputs:
+//      - Nothing
